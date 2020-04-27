89dfbf5f85cd40df5141127c42708a1ff5279423
ReanSchwarzer1 <42261295+ReanSchwarzer1@users.noreply.github.com>
2020-04-27 15:39:27 -0400

    2nd Commit @reanschwarzer1 modified from @raghu-raj23 repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  fname = '';
+  lname = '';
+  email = '';
+  pass = '';
+  conpass = '';
+  dob = '';
+
+  constructor(private fb: FormBuilder) {
+
+    this.rForm = fb.group({
+      fname: [null, Validators.required],
+      lname: [null, Validators.compose([Validators.required, Validators.minLength(2)])],
+      email : [null, Validators.required],
+      pass : [null, Validators.required],
+      conpass : [null, Validators.required],
+      dob : [null, Validators.required],
+      validate : ''
+    });
+
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+
+    this.rForm.get('validate').valueChanges.subscribe(
+      (validate) => {
+        if (validate === '1') {
+          this.rForm.get('fname').setValidators([Validators.required, Validators.minLength(3)]);
+
+        } else {
+          this.rForm.get('fname').setValidators(Validators.required);
+        }
+        this.rForm.get('fname').updateValueAndValidity();
+      }
+    );
+  }
+
+  addPost(post) {
+    this.lname = post.lname;
+    this.fname = post.fname;
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SlideshowLoginComponent } from './slideshow-login/slideshow-login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SlideshowLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
