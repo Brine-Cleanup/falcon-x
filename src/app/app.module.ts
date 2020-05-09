977e7a9cdfad41420bf7197649a391bdd93af0c3
Yakshchopra <yakshchopra@gmail.com>
2020-05-09 20:31:49 +0530

    landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingComponent } from './landing/landing.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';  
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateAccountComponent,
     NavBarComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
